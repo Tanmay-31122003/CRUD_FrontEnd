99bc652754dbfdb793ef05873607f5778597053d
Tanmay-31122003 <143332069+Tanmay-31122003@users.noreply.github.com>
2024-09-12 18:08:18 +0530

    handle error and cache in user service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
@@ -11,36 +11,44 @@ export class UserService {
   private apiUrl = "https://crud-server-green.vercel.app";
   httpClient = inject(HttpClient);
 
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate', 
+      'Pragma': 'no-cache', 
+      'Expires': '0'
+    })
+  };
+
   getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(`${this.apiUrl}/users`)
+    return this.httpClient.get<User[]>(`${this.apiUrl}/users`, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
   }
 
   getUser(id: string): Observable<User> {
-    return this.httpClient.get<User>(`${this.apiUrl}/users/${id}`)
+    return this.httpClient.get<User>(`${this.apiUrl}/users/${id}`, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
   }
 
   addUser(user: User): Observable<User> {
-    return this.httpClient.post<User>(`${this.apiUrl}/users`, user)
+    return this.httpClient.post<User>(`${this.apiUrl}/users`, user, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
   }
 
   updateUser(id: string, user: User): Observable<User> {
-    return this.httpClient.put<User>(`${this.apiUrl}/users/${id}`, user)
+    return this.httpClient.put<User>(`${this.apiUrl}/users/${id}`, user, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
   }
 
   deleteUser(id: string): Observable<void> {
-    return this.httpClient.delete<void>(`${this.apiUrl}/users/${id}`)
+    return this.httpClient.delete<void>(`${this.apiUrl}/users/${id}`, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
